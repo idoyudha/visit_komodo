80ec65c7d99c6fd1854711f8e414c2e372bfe642
idoyudha <idowidya.yudhatama@gmail.com>
2021-03-21 12:43:28 +0700

    Little update for js profile

---
+++ visit_komodo/static/js/index.js
@@ -0,0 +1,9 @@
+function showForm() {
+    let val = document.getElementById('profile-form')
+    if (val.style.display === 'none') {
+        val.style.display = 'block'
+    } 
+    else {
+        val.style.display = 'none'
+    }
+}
