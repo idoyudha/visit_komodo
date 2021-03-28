aa8029b9234321d99cca682b810c1e04b6bd6345
idoyudha <idowidya.yudhatama@gmail.com>
2021-03-28 22:28:01 +0700

    Add fetch, need further update

--- visit_komodo/static/js/index.js
+++ visit_komodo/static/js/index.js
@@ -1,4 +1,4 @@
-function addtoWatchlist(id) {
+function addtoWishlist(id) {
     let text = document.getElementById('wishlist').textContent
     let type = document.getElementById('type').textContent
     console.log(type, id)
@@ -10,5 +10,31 @@ function addtoWatchlist(id) {
         document.getElementById('wishlist').className = 'btn btn-danger'
         document.getElementById('wishlist').textContent = 'Add to wishlist'
     }
-    fetch()
+    if (type === 'destination') {
+        fetch(`/destination_api_detail/${id}`)
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error))
+    }
+    else if (type === 'food') {
+        fetch(`/food_api_detail/${id}`)
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error))
+    }
+    else if (type == 'event') {
+        fetch(`/event_api_detail/${id}`)
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error))
+    }
+    else if (type == 'travel guide') {
+        fetch(`/travelguide_detail/${id}`)
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error))
+    }
+    else {
+        console.log('wrong type')
+    }
 }
